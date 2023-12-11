34cc518fa6fa9fd5b48968811743c3db487e259b
Panjun-Kwon <pndlm1979@naver.com>
2023-12-11 11:53:18 +0900

    JS-Projects

--- src/projects.js
+++ src/projects.js
@@ -1,3 +1,35 @@
 'use strict';
 
-console.log(header);
+// 프로젝트 필터링 관련 로직 처리
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project');
+const projcetsContainer = document.querySelector('.projects');
+categories.addEventListener('click', (event) => {
+  const filter = event.target.dataset.category;
+  if (filter == null) {
+    return;
+  }
+
+  handleActiveSelection(event.target);
+  filterProjects(filter);
+});
+
+function handleActiveSelection(target) {
+  const active = document.querySelector('.category--selected');
+  active.classList.remove('category--selected');
+  target.classList.add('category--selected');
+}
+
+function filterProjects(filter) {
+  projcetsContainer.classList.add('anim-out');
+  projects.forEach((proejct) => {
+    if (filter === 'all' || filter === proejct.dataset.type) {
+      proejct.style.display = 'block';
+    } else {
+      proejct.style.display = 'none';
+    }
+  });
+  setTimeout(() => {
+    projcetsContainer.classList.remove('anim-out');
+  }, 250);
+}
